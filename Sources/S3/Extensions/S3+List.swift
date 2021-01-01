7f58fd7ca885af135e07c96b12aaec8655a8c08f
Jimmy McDermott <mcdappdev@gmail.com>
2021-01-01 14:36:49 -0600

    Prefix

--- Sources/S3/Extensions/S3+List.swift
+++ Sources/S3/Extensions/S3+List.swift
@@ -12,7 +12,11 @@ import Foundation
 extension S3 {
     
     /// Get list of objects
-    public func list(bucket: String, region: Region? = nil, headers: HTTPHeaders) throws -> EventLoopFuture<BucketResults> {
+    public func list(bucket: String,
+                     region: Region? = nil,
+                     prefix: String? = nil,
+                     headers: HTTPHeaders
+    ) throws -> EventLoopFuture<BucketResults> {
         let region = region ?? signer.config.region
         guard let baseUrl = URL(string: region.hostUrlString(bucket: bucket)), let host = baseUrl.host,
             var components = URLComponents(string: baseUrl.absoluteString) else {
@@ -21,21 +25,18 @@ extension S3 {
         components.queryItems = [
             URLQueryItem(name: "list-type", value: "2")
         ]
+        if let prefix = prefix {
+            components.queryItems?.append(URLQueryItem(name: "prefix", value: prefix))
+        }
         guard let url = components.url else {
             throw S3.Error.invalidUrl
         }
         var headers = headers
         headers.replaceOrAdd(name: "host", value: host)
         let awsHeaders = try signer.headers(for: .GET, urlString: url, region: region, bucket: bucket, headers: headers, payload: .none)
-        return try make(request: url, method: .GET, headers: awsHeaders, data: emptyData()).flatMapThrowing { response -> BucketResults in
+        return try make(request: url, method: .GET, headers: awsHeaders, data: emptyData()).flatMapThrowing { response in
             try self.check(response)
             return try response.decode(to: BucketResults.self)
         }
     }
-    
-    /// Get list of objects
-    public func list(bucket: String, region: Region? = nil) throws -> EventLoopFuture<BucketResults> {
-        return try list(bucket: bucket, region: region, headers: [:])
-    }
-    
 }
